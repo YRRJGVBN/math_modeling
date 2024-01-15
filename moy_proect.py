67691bfcdc68316f214785c3488102b8e8afa94e
YRRJGVBN <146557908+YRRJGVBN@users.noreply.github.com>
2024-01-15 19:03:39 +0000

    ничего не понятно

---
+++ moy_proect.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+xdata = []
+def circle_move(R, vx0, time):
+    x0 = vx0 * time
+    alpha = np.arange(0, 2*np.pi, 0.1)
+    x = x0  + R*np.cos(alpha)
+    y = R*np.sin(alpha)
+    return x, y
+def animate(i):
+    ball.set_data(circle_move(R=0, vx0=0.1, time=i))
+if __name__ == '__main__':
+ 
+    fig, ax = plt.subplots()
+    ball, = plt.plot([], [], 'o', color='r', label='Ball')
+ 
+    edge = 10
+    plt.axis('equal')
+    ax.set_xlim(-edge, edge)
+    ax.set_ylim(-edge, edge)
+    
+    ani = FuncAnimation(fig,
+                        animate,
+                        frames=100,
+                        interval=30
+                       )
+ 
+    ani.save('animation_3.gif') 
