e4e510f716a543c30965f4165129b45742d6d7ad
YRRJGVBN <146557908+YRRJGVBN@users.noreply.github.com>
2024-04-16 14:24:05 +0000

    лллллл

--- prv2.py
+++ prv2.py
@@ -2,20 +2,38 @@ def worgle(word, Tword):
     zn = []
     w = list(word)
     Tw = list(Tword)
-    
-    for i in w:
-        if i in Tw:
-            zn.append('#')
-        else:
-            zn.append('x')
 
-    for i in range(5):
-        if w[i] == Tw[i]:
-            zn[i] == '0'
+    if len(w) != 5:
+        print('Вводите только слова из 5 букв')
+        return 0
+    else:
+        for i in w:
+            if i in Tw:
+                zn.append('$')
+            else:
+                zn.append('X')
 
-    print(zn)
+        for i in range(5):
+            if w[i] == Tw[i]:
+                zn[i] = '0'
+        r = ''.join(zn)
+        print(word)
+        print(r)
+        return 1
 
-
-word = 'табак'
+m = 0
 Tword = 'барак'
-worgle(word, Tword)
+
+for i in range(6):
+    word = input()
+    worgle(word, Tword)
+    m += 1
+
+    if m == 6:
+        print(f'Вы проиграли! Загаданное слово: {Tword}')
+        break
+    
+    if word == Tword and m != 6:
+        print(f'Вы отгадали слово {Tword}')
+        m = 0
+        break
